2722517ae871e1aca7b0a949239564ba11dabfa7
Nobu19800 <TyouKyozyakuTaisitu@yahoo.co.jp>
2014-09-05 08:07:57 +0900

    807

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#-adsh_path_var PATH,OOoScriptPath,OOoPath
+#-adsh_path_var PATH,OOoScriptPath,OOoPath,OpenOfficePath
 
 OOoScriptPath=~/.openoffice.org/3/user/Scripts/python
 
 OOoPath=/opt/openoffice3/basis-link/program
+
+OpenOfficePath=/opt/openoffice3/program
 
 if [ ! -e $OOoPath ]; then
 	OOoPath=/usr/lib/openoffice/basis-link/program
@@ -19,16 +21,25 @@ cp OOoImpressRTC.py ${OOoScriptPath}/OOoImpressRTC.py
 
 
 
+sudo cp rtc.conf ${OOoPath}/rtc.conf
+
 cp OOoRTC -r ~/OOoRTC
 
 
-sudo cp rtc.conf ${OOoPath}/rtc.conf
 
-unopkg add -v OOoCalcControlRTC.oxt
-unopkg add -v OOoDrawControlRTC.oxt
-unopkg add -v OOoWriterControlRTC.oxt
-unopkg add -v OOoBaseControlRTC.oxt
-unopkg add -v OOoImpressControlRTC.oxt
+if [ -e $OpenOfficePath ]; then
+	${OpenOfficePath}/unopkg add -v OOoCalcControlRTC.oxt
+	${OpenOfficePath}/unopkg add -v OOoDrawControlRTC.oxt
+	${OpenOfficePath}/unopkg add -v OOoWriterControlRTC.oxt
+	${OpenOfficePath}/unopkg add -v OOoBaseControlRTC.oxt
+	${OpenOfficePath}/unopkg add -v OOoImpressControlRTC.oxt
+else
+	unopkg add -v OOoCalcControlRTC.oxt
+	unopkg add -v OOoDrawControlRTC.oxt
+	unopkg add -v OOoWriterControlRTC.oxt
+	unopkg add -v OOoBaseControlRTC.oxt
+	unopkg add -v OOoImpressControlRTC.oxt
+fi
 
 
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,10 @@
-#-adsh_path_var PATH,OOoScriptPath,OOoPath
+#-adsh_path_var PATH,OOoScriptPath,OOoPath,OpenOfficePath
 
 OOoScriptPath=~/.openoffice.org/3/user/Scripts/python
 
 OOoPath=/opt/openoffice3/basis-link/program
+
+OpenOfficePath=/opt/openoffice3/program
 
 if [ ! -e $OOoPath ]; then
 	OOoPath=/usr/lib/openoffice/basis-link/program
@@ -17,17 +19,28 @@ rm ${OOoScriptPath}/OOoBaseRTC.py
 rm ${OOoScriptPath}/OOoImpressRTC.py
 
 
-rm -rf ~/OOoRTC
+
 
 
 
 sudo rm ${OOoPath}/rtc.conf
 
-unopkg remove -v OOoCalcControlRTC.oxt
-unopkg remove -v OOoDrawControlRTC.oxt
-unopkg remove -v OOoWriterControlRTC.oxt
-unopkg remove -v OOoBaseControlRTC.oxt
-unopkg remove -v OOoImpressControlRTC.oxt
+rm -rf ~/OOoRTC
+
+
+if [ -e $OpenOfficePath ]; then
+	${OpenOfficePath}/unopkg remove -v OOoCalcControlRTC.oxt
+	${OpenOfficePath}/unopkg remove -v OOoDrawControlRTC.oxt
+	${OpenOfficePath}/unopkg remove -v OOoWriterControlRTC.oxt
+	${OpenOfficePath}/unopkg remove -v OOoBaseControlRTC.oxt
+	${OpenOfficePath}/unopkg remove -v OOoImpressControlRTC.oxt
+else
+	unopkg remove -v OOoCalcControlRTC.oxt
+	unopkg remove -v OOoDrawControlRTC.oxt
+	unopkg remove -v OOoWriterControlRTC.oxt
+	unopkg remove -v OOoBaseControlRTC.oxt
+	unopkg remove -v OOoImpressControlRTC.oxt
+fi
 
 
 
