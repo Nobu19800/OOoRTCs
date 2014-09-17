97c46e51cf5d7310306c69421d2e8b4b82ea6071
Nobu19800 <TyouKyozyakuTaisitu@yahoo.co.jp>
2014-09-18 04:56:39 +0900

    456

--- unoInstall.bat
+++ unoInstall.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 
-set OOoScriptPath=%USERPROFILE%\AppData\Roaming\OpenOffice.org\3\user\Scripts\python
+
 set OOoPath="%ProgramFiles%\OpenOffice.org 3\program"
 
 IF NOT EXIST %OOoPath% (
@@ -8,26 +8,21 @@ IF NOT EXIST %OOoPath% (
    set OOoPath="%ProgramFiles(x86)%\OpenOffice.org 3\program"
 )
 
-del %OOoScriptPath%\OOoCalcRTC.py
-del %OOoScriptPath%\OOoDrawRTC.py
-del %OOoScriptPath%\OOoWriterRTC.py
-del %OOoScriptPath%\OOoBaseRTC.py
-del %OOoScriptPath%\OOoImpressRTC.py
-
-
-del %OOoPath%\OOoRTC.py
-del %OOoPath%\rtc.conf
-rd /s %OOoPath%\OOoRTC
-
 
 set OOoCD=%CD%
 
 cd %OOoPath%
+
 unopkg remove -v OOoCalcControlRTC.oxt
 unopkg remove -v OOoDrawControlRTC.oxt
 unopkg remove -v OOoWriterControlRTC.oxt
 unopkg remove -v OOoBaseControlRTC.oxt
 unopkg remove -v OOoImpressControlRTC.oxt
 
-pause
+unopkg add -v "%OOoCD%\OOoCalcControlRTC.oxt"
+unopkg add -v "%OOoCD%\OOoDrawControlRTC.oxt"
+unopkg add -v "%OOoCD%\OOoWriterControlRTC.oxt"
+unopkg add -v "%OOoCD%\OOoBaseControlRTC.oxt"
+unopkg add -v "%OOoCD%\OOoImpressControlRTC.oxt"
+
 
